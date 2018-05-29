fb1201aff255bd9f3a04ec9e2f11cfacdafd197d
Nunky Lado <puatachok@gmail.com>
2018-05-29 13:29:48 +0300

    Updating

--- app/src/main/java/com/example/lado/getipadress/MainActivity.java
+++ app/src/main/java/com/example/lado/getipadress/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.lado.getipadress;
 
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +19,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +33,24 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.getIP);
         Button press = (Button) findViewById(R.id.press);
+
+        //make shortcut with ShortcutManager
+        ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+
+        //making Intent
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        Uri uri = intent.getData();
+
+        //show information when we pressed long on icon
+        ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "id1")
+                .setShortLabel("Task")
+                .setLongLabel("Get IP Address")
+                .setIcon(Icon.createWithResource(MainActivity.this, R.drawable.ic_launcher_round))
+                .setIntent(new Intent(Intent.ACTION_VIEW, uri))
+                .build();
+
+        //make array with our inforamtion
+        shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut));
 
         press.setOnClickListener(new View.OnClickListener() {
             @Override
